0885221d6c12f11cea6f545084144236e6981689
user1303836 <gcvvvvvv@gmail.com>
2024-10-10 13:37:40 -0400

    ......

--- run.go
+++ run.go
@@ -2,6 +2,15 @@ package main
 
 import "net/http"
 
+type DownloadRequest struct {
+	URL string `json:"url"`
+}
+
+type DownloadResponse struct {
+	DownloadID string `json:"download_id"`
+	Status     string `json:"status"`
+}
+
 func main() {
 	http.HandleFunc("/api/download", handleDownloadRequest)
 	http.HandleFunc("/download/", serveDownload)
